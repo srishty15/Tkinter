dddfd93c0e8c4ed60589231e60f6699757762d23
Srishty <srishty1510@gmail.com>
2018-08-25 04:44:35 +0530

    Update 01. Uploading Image.py

--- 01. Uploading Image.py
+++ 01. Uploading Image.py
@@ -12,7 +12,7 @@ file_path = filedialog.askopenfilename(filetypes=[(("Picture File","*.jpg;*.png;
 img=Image.open(file_path)
 file_names=str(int(time.time()))
 print(file_names)
-img.save("path/image_name.png")  #specify the path in 'path' and image will be saved as image_name.png
+img.save("path/image_name.png")  #specify the path in 'path' and image will be saved as image_name.png ; You can edit the image_name to any name 
 tkimage=ImageTk.PhotoImage(img)
 root.mainloop()
 

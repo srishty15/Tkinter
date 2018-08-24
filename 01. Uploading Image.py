b7e33067b9bd87cd04dc5eb96bd50f890ef4ac18
Srishty <srishty1510@gmail.com>
2018-08-25 04:41:32 +0530

    Update 01. Uploading Image.py

--- 01. Uploading Image.py
+++ 01. Uploading Image.py
@@ -7,7 +7,7 @@ from PIL import Image
 from PIL import ImageTk
 
 root = Tk()
-
+root.withdraw()
 file_path = filedialog.askopenfilename(filetypes=[(("Picture File","*.jpg;*.png;*.gif"))])
 img=Image.open(file_path)
 file_names=str(int(time.time()))
@@ -15,3 +15,10 @@ print(file_names)
 img.save("path/image_name.png")
 tkimage=ImageTk.PhotoImage(img)
 root.mainloop()
+root1 = Tk()
+    
+    file_path = filedialog.askopenfilename(filetypes=[(("Picture File", "*.jpg;*.png"))])
+    img = Image.open(file_path)
+    img.save("C:/Users/9911v/PycharmProjects/Facial Emotion Detector/abc.png")
+    tkimage = ImageTk.PhotoImage(img)
+    root1.mainloop()
